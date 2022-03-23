55db2b5efa6187f86d266386f19a2f892cf06340
Debjani97 <debjanicob97@gmail.com>
2022-03-23 16:18:20 +0530

    final_done

--- src/controller/controllers.js
+++ src/controller/controllers.js
@@ -98,49 +98,49 @@ const college = async function (req, res) {
 
 const getCollege = async function (req, res) {
      try{
-          const collegeName = req.query.collegeName
-       
-          if(!collegeName){
-               return res.status(400).send({status:false, msg:"BAD REQUEST please provied valid collegeName"})}
- 
-          const college =await collegemodel.find({ name:collegeName, isDeleted: false })
-          if (!college) {
-             return res.status(404).send({ status: false, msg: "BAD REQUEST  college not found" })
-           }
-            console.log(college)
-           const collegeId = college[0]._id
-         //   delete req.body["collegeName"]
-           
-             const interName = await internmodel.find({collegeId: collegeId, isDeleted : false})
-             if(interName.length <= 0){
-                  res.status(404).send({msg: `No intern apply for this college: ${college} `})}
-             const interests =[]
-       
-             for (let i=0; i<interName.length;i++)
-             {
-                 let Object={}
-                 Object._id = interName[i]._id
-                 Object.name=interName[i].name
-                 Object.email = interName[i].email
-                 Object.mobile=interName[i].mobile
-                 interests.push(Object)
-             }
-       
-             const ObjectData = {
-                 name:college[0].name,
-                 fullName:college[0].fullName,
-                 logoLink:college[0].logoLink,
-                 interests:interests
-             }
-             
-           return res.status(201).send({ status: true, count : interests.length, msg:ObjectData })
-       
-       
-       }
-       catch (err) {
-         return res.status(500).send({ status: false, msg: err.message })
-       }
-       }
+         const collegeName = req.query.collegeName
+      
+         if(!collegeName){
+              return res.status(400).send({status:false, msg:"BAD REQUEST please provied valid collegeName"})}
+
+         const college =await collegemodel.find({ name:collegeName, isDeleted: false })
+         if (!(college).length>0) {
+            return res.status(404).send({ status: false, msg: "BAD REQUEST  college not found" })
+          }
+           console.log(college)
+          const collegeId = college[0]._id
+        //   delete req.body["collegeName"]
+          
+            const interName = await internmodel.find({collegeId: collegeId, isDeleted : false})
+            if(interName.length <= 0){
+                 res.status(404).send({msg: `No intern apply for this college: ${college} `})}
+            const interests =[]
+      
+            for (let i=0; i<interName.length;i++)
+            {
+                let Object={}
+                Object._id = interName[i]._id
+                Object.name=interName[i].name
+                Object.email = interName[i].email
+                Object.mobile=interName[i].mobile
+                interests.push(Object)
+            }
+      
+            const ObjectData = {
+                name:college[0].name,
+                fullName:college[0].fullName,
+                logoLink:college[0].logoLink,
+                interests:interests
+            }
+            
+          return res.status(200).send({ status: true, count : interests.length, msg:ObjectData })
+      
+      
+      }
+      catch (err) {
+        return res.status(500).send({ status: false, msg: err.message })
+      }
+      }
 
 module.exports.college=college
 module.exports.intern=intern
